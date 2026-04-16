836c25004c1a7b60308dc511b23700e3d92e8f7a
karnkeshav <160194487+karnkeshav@users.noreply.github.com>
2026-04-16 17:28:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -30,7 +30,14 @@ export async function initializeServices() {
       }
 
       const cfg = window.__firebase_config;
-      const autoCfg = window.__automation_firebase_config;
+      const autoCfg = window.__automation_firebase_config = {
+    apiKey: "AIzaSyBibeP6f_OWGLgorKIx_1D_qXZfDKcURNM",
+    authDomain: "ready4exam-automation.firebaseapp.com",
+    projectId: "ready4exam-automation",
+    storageBucket: "ready4exam-automation.appspot.com", // Ensure this matches your PDF storage
+    messagingSenderId: "642756751654",
+    appId: "1:642756751654:web:15a64377c207fdb5a0d1e7"
+};
 
       if (!cfg?.apiKey) throw new Error("Master Firebase config missing");
 
